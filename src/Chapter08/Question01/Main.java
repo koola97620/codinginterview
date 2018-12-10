f316dee23fcd574f277de203a9a1a940dc104ee6
Jaeyong <koola97620@nate.com>
2018-12-10 21:56:54 +0900

    181210. CodingInterview , 8-1

--- src/Chapter08/Question01/Main.java
+++ src/Chapter08/Question01/Main.java
@@ -22,13 +22,11 @@ public class Main {
     }
 
     public static int downStairs(int n) {
-        if(n==0) {
-            return 1;
+        if(n<0) {
+            return 0;
         }
-        else if(n==1)
+        else if(n==0)
             return 1;
-        else if(n==2)
-            return 2;
         else{
             return downStairs(n-1) + downStairs(n-2) + downStairs(n-3);
         }
