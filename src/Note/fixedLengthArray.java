69d4122f1f0076f5fbdde55838b8c8b936bbb6e3
Jaeyong <koola97620@nate.com>
2018-12-19 21:30:46 +0900

    181219. 연습파일.

---
+++ src/Note/fixedLengthArray.java
@@ -0,0 +1,14 @@
+package Note;
+
+/*
+  정해진 길이 안에.
+  WH 두 문자로만 이루어진. 조합의 배열을 구해라.
+  길이가 2면
+  WH, HW
+  3이면
+  WWW, WWH, WHH, WHW, HHH, HWA, HWW,등등.이런거 다 구해!
+ */
+
+public class fixedLengthArray {
+
+}
